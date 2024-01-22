71744058c8f64eed02143421981a65070afc2925
Azzam <samarafc46@gmail.com>
2024-01-22 13:58:04 +0700

    first-commit

---
+++ css/js/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const navbarFixed = header.offsetTop;
+
+    if(window.pageYOffset > navbarFixed){
+       header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+};
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu')
+humberger.addEventListener('click',function(){
+    humberger.classList.toggle('active');
+    navMenu.classList.toggle('hidden');
+})
